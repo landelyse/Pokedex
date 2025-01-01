b2b21d9121c17467373343df6f590a1a8d93407e
jbnong07 <jbnong07@naver.com>
2025-01-02 00:17:41 +0900

    🔥 [Remove] stroyboard

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by 박진홍 on 12/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
